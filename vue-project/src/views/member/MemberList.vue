3df409e020a238bb000ec41c944b4f3e81cfb97f
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-15 18:17:32 +0900

    MemberList.vueにルートパラメータ設定

--- vue-project/src/views/member/MemberList.vue
+++ vue-project/src/views/member/MemberList.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
 import {inject} from "vue";
-/*import {RouterLink} from "vue";
+import {RouterLink} from "vue-router";
 import type {Member} from "@/interfaces";
 const memberList = inject("memberlist") as Map<number, Member>;
-*/
+
 </script>
 <template>
     <h1>会員管理</h1>
+    <nav id="breadcrumb">
+        <ul>
+            <li>
+                <RouterLink v-bind:to="{name: 'AppTop'}">
+                    TOP
+                </RouterLink>
+            </li>
+            <li>会員リスト</li>
+        </ul>
+    </nav>
     <section>
         <h2>会員リスト</h2>
+        <section>
+            <ul>
+                <li
+                    v-for="[id, member] in memberList"
+                    v-bind:key="id">
+                    <RouterLink v-bind:to="{name: 'MemberDeetail', paramas: {id: id}}">
+                        IDが{{ id }}の{{ member.name }}さん
+                    </RouterLink>
+                </li>
+            </ul>
+        </section>
     </section>
 </template>
